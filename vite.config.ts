1e01228400522ede61969bbfe314db2521e0b27b
IT-001\41629 <41629320@qq.com>
2026-08-12 13:35:18 +0800

    修改

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,41 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-  test: {
-    environment: 'jsdom',
-  },
-  base: '/ai-wild-animals/',
-  build: {
-    sourcemap: 'hidden',
-  },
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          'react-dev-locator',
-        ],
-      },
-    }),
-    tsconfigPaths()
-  ],
-  server: {
-    port: 5177,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8082',
-        changeOrigin: true,
-      },
-      '/static': {
-        target: 'http://127.0.0.1:8082',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    test: {
+      environment: 'jsdom',
+    },
+    base: env.VITE_BASE_PATH || '/',
+    build: {
+      sourcemap: 'hidden',
+    },
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            'react-dev-locator',
+          ],
+        },
+      }),
+      tsconfigPaths()
+    ],
+    server: {
+      port: 5177,
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:8082',
+          changeOrigin: true,
+        },
+        '/static': {
+          target: 'http://127.0.0.1:8082',
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 })
